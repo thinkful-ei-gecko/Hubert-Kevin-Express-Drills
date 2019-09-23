0225c1607b6498e6d7dd2666856623533cbc77a8
Hubert Yang <offspring7890@yahoo.com>
2019-09-23 13:48:28 -0700

    finish drill #2

--- app.js
+++ app.js
@@ -9,6 +9,25 @@ app.get('/sum', (req, res) => {
   res.send(total);
 })
 
+// a=97, z=122
+app.get('/cipher', (req, res) => {
+  const text = req.query.text.toLowerCase();
+  const shift = Number(req.query.shift);
+
+  const convertText = text.split(' ').map(word => word.split('').map(char => char.charCodeAt(0)))
+  const textConverter = convertText.map(wordArr => {
+    let modifyWord = wordArr.map(charCode => {
+      if(charCode >= 122) {
+        return 'a';
+      }
+      return String.fromCharCode(charCode + shift)
+    })
+    return modifyWord.join('');
+  });
+  let wordResult = textConverter.join(' ');
+  res.send(wordResult)
+})
+
 app.listen(8000, () => {
   console.log('Express server is running!')
 })
